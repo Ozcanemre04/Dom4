9271ca8d8992a961bcab0e86ac7144425aaa4943
ozcanemre <ozcan.emre1608@gmail.com>
2022-04-22 10:29:51 +0200

    m

--- script.js
+++ script.js
@@ -13,19 +13,14 @@ let main = document.querySelector('main');
 let section2 = main.children[1];
 let section3 = main.children[2]
 
-
+let h22 = section3.children[0]
 let h21 = section2.children[0]
-console.log(h21);
-let div = section3.children[0]
-let h22 = div.children[0]
-console.log(h22);
 
+section3.appendChild(h21)
+section2.appendChild(h22)
 
-div.appendChild(h21);
-section2.appendChild(h22);
-
-
-
+let p = section2.children[0]
+section3.appendChild(p)
 /*exercice 3 */ 
 
 main.removeChild(section3)
